b28a0a15b7a1f93a57b353208c30f6fe0984f514
Raul de Melo Tavares <131861460+Shyzin@users.noreply.github.com>
2024-06-06 18:34:12 +0000

    mandei o link pra minha futura esposa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Nao ta rodando</h1>   
+    return <h1>VOCE NAO ESTA SOZINHA, EU ESTOU AQUI COM VOCE AGORA, VC E A MULHER DA MINHA VIDA, ja consegui um link na internet amor</h1>   
 }
 
 export default Home;
