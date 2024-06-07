b1b6a8e8862137503cc0170435478b9decaddf78
Raul de Melo Tavares <131861460+Shyzin@users.noreply.github.com>
2024-06-07 21:58:31 +0000

    Fazendo a formatacao do codigo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>VOCE NAO ESTA SOZINHA, EU ESTOU AQUI COM VOCE AGORA, VC E A MULHER DA MINHA VIDA, ja consegui um link na internet amor</h1>   
+function Home() {
+  return 
+    <h1>
+      VOCE NAO ESTA SOZINHA, EU ESTOU AQUI COM VOCE AGORA, VC E A MULHER DA
+      MINHA VIDA, ja consegui um link na internet amor
+    </h1>
+}
+
+function teste() {
+  console.log("Teste");
 }
 
 export default Home;
--- pages/pages.js
+++ pages/pages.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Nao ta rodando</h1>   
+function Home() {
+  return <h1>Nao ta rodando</h1>;
 }
 
 export default Home;
