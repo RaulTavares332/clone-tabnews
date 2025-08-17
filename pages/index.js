fb5f60fcc8e84e74ed04d04ba6d256a2425feec1
Raul de Melo Tavares <131861460+RaulTavares332@users.noreply.github.com>
2025-08-17 19:22:41 +0000

    add `api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-  return;
-  <h1>
-    VOCE NAO ESTA SOZINHA, EU ESTOU AQUI COM VOCE AGORA, VC E A MULHER DA MINHA
-    VIDA, ja consegui um link na internet amor
-  </h1>;
+  return (
+    <h1>
+      VOCE NAO ESTA SOZINHA, EU ESTOU AQUI COM VOCE AGORA, VC E A MULHER DA
+      MINHA VIDA, ja consegui um link na internet amor
+    </h1>
+  );
 }
 
 function teste() {
